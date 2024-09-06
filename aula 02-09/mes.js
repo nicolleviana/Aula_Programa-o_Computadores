10c7fcdda9fa619f1470f2526745c0dbb93cd887
nicolleviana <vianaigaracu@gmail.com>
2024-09-05 21:04:59 -0300

    mes.js

---
+++ aula 02-09/mes.js
@@ -0,0 +1,41 @@
+var mesAno = parseInt(prompt("Digite um número de 1 a 12: "));
+
+if(mesAno ==1){
+    console.log("1 é Janeiro");
+}
+else if(mesAno == 2){
+    console.log("2 é Fevereiro");
+    }
+    else if(mesAno == 3){
+    console.log("3 é Março");
+    }
+    else if(mesAno == 4){
+    console.log("4 é Abril");
+    }
+    else if(mesAno == 5){
+    console.log("5 é Maio");
+    }
+    else if(mesAno == 6){
+    console.log("6 é Junho");
+    }
+    else if(mesAno == 7){
+    console.log("7 é Julho");
+    }
+    else if(mesAno == 8){
+        console.log("8 é Agosto");
+        }
+        else if(mesAno == 9){
+        console.log("9 é Setembro");
+        }
+        else if(mesAno == 10){
+        console.log("10 é Outubro");
+        }
+        else if(mesAno == 11){
+        console.log("11 é Novembro");
+        }
+        else if(mesAno == 12){
+        console.log("12 é Dezembro");
+        }
+        else(mesAno == 13){
+        console.log("Digite do 1 a 12 TONTO");
+        }
