7f2f509571987c50644b5261679ce40ff0ca32d3
nicolleviana <vianaigaracu@gmail.com>
2024-09-09 22:24:00 -0300

    lista de exercícios

---
+++ aula 09-09/exercicio4.js
@@ -0,0 +1,12 @@
+/*
+O cardápio de uma lanchonete é o seguinte:
+Especificação Preço unitário 
+100 Cachorro quente 1,10 
+101 Bauru simples 1,30 
+102 Bauru c/ovo 1,50 
+103 Hamburger 1,10 
+104 Cheeseburger 1,30 
+
+Escrever um algoritmo que leia o código do lanche pedido, a quantidade e calcule o valor a ser pago por aquele lanche. 
+*/
+
---
+++ aula 09-09/exercicio5.js
@@ -0,0 +1,10 @@
+/*
+Elabore um algoritmo que calcule o valor a ser pago por um produto considerando a quantidade, o preço e a escolha da condição de pagamento. 
+Utilize os códigos da tabela a seguir e efetuar o cálculo adequado.
+Código =>  Condição Pagamento
+1      => À vista, dinheiro ou cheque, 10% de desconto
+2      => À vista, cartão de credito, 5% de desconto
+3      => Em 2 vezes, preço sem juros
+4      => Em 3 vezes, preço + 10% de juros
+*/
+
