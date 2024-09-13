edded5a310a6fae3c510464994eff651d0769012
nicolleviana <vianaigaracu@gmail.com>
2024-09-12 22:26:08 -0300

    aula sobre pizza e bebida

--- aula 09-09/exercicio4.js
+++ aula 09-09/exercicio4.js
@@ -10,13 +10,36 @@ Especificação Preço unitário
 Escrever um algoritmo que leia o código do lanche pedido, a quantidade e calcule o valor a ser pago por aquele lanche. 
 */
 
-var codigoLanche = parseInt(prompt("Digite o código do lanche: "));
-var quantidade = parseInt(prompt("Digite a quantidade que deseja: "));
+var descricaoCardapio = "==== ESCOLHA UM LANCHE ====\n";
+descricaoCardapio = descricaoCardapio + "Cód | Nome            | Preço";
+descricaoCardapio += "100 | Cachorro quente | 1,10\n";
+descricaoCardapio += "101 | Bauru simples   | 1,30\n";
+descricaoCardapio += "102 | Bauru c/ ovo    | 1,50\n";
+descricaoCardapio += "103 | Hamburger       | 1,10\n";
+descricaoCardapio += "104 | Cheeseburger    | 1,30\n";
+descricaoCardapio += "\n Digite o CÓDIGO do Lanche Escolhido: ";
 
-if (cardapio[codigoLanche]){
-    var precoTotal = cardapio[codigoLanche] + quantidade;
-    console.log("O valor a ser pago é: R$" + precoTotal); 
-} else{
-    console.log("Código de lanche inválido!");
+var codigoLanche = parseInt(prompt(descricaoCardapio));
+
+var quantidade = parseInt(prompt("Quantos lanches você deseja pedir? "));
+
+var valorTotal = 0
+
+if(codigoLanche == 100){
+   valorTotal = quantidade * 1.10;
+}else if(codigoLanche == 101){
+    valorTotal = quantidade * 1.30;
+}else if(codigoLanche == 102){
+    valorTotal = quantidade * 1.50;
+}else if(codigoLanche == 103){
+    valorTotal = quantidade * 1.10;
+}else if(codigoLanche == 104){
+    valorTotal = quantidade* 1.30;
+}else{
+    console.log("Código de lanche inválido!!");
 }
 
+console.log("Seu pedido do lanche [ " + codigoLanche + " ]");
+console.log("Foram pedidos [ " + quantidade + " ] lanches");
+console.log("Total a pagar:R$ " , valorTotal);
+   
--- aula 09-09/exercico7.js
+++ aula 09-09/exercico7.js
@@ -15,12 +15,67 @@ Apresente o Menu de Bebidas:
 Solicite ao usuário qual Pizza e quantidade que deseja comprar, qual refrigerante e quantidade, ao final apresente o total a pagar.
 */
 
-var pizza = parseInt(prompt("Digite o número da pizza desejada: "));
-var  quantidadePizza = parseInt(prompt("Digite a quantidade desejada: "));
-var totalPizza = pizza * quantidadePizza;
+var menuPizza = "==== ESCOLHA UMA PIZZA ====\n";
+menuPizza = menuPizza + "Numero | Nome | Preço\n";
+menuPizza += "1 | Mussarela  | 20,00 \n";
+menuPizza += "2 | Calabresa  | 25,00 \n";
+menuPizza += "3 | Marguerita | 23,00 \n";
+menuPizza += "4 | 4 Queijos  | 29,99\n";
+menuPizza += "5 | Baiana     | 22,50\n";
+menuPizza += "\n Digite o NUMERO da Pizza Escolhida: ";
 
+var numeroPizza = parseInt(prompt(menuPizza));
 
+var quantidade = parseInt(prompt("Quantas pizzas você deseja pedir? "));
 
-var bebida = parseInt(prompt("Digite o número da sua bebida: "));
-var quantidadeBebida = parseInt(prompt("Digite a qauntidade desejada: ")); 
+var totalPizza = 0
 
+var menuBebida = "==== ESCOLHA UMA BEBIDA ====\n";
+menuBebida = menuBebida + "Numero | Nome | Preço\n";
+menuBebida += "1 | Refrigerante Lata  | 5,00 \n";
+menuBebida += "2 | Refrigerante 2L    | 10,00 \n";
+menuBebida += "3 | Agua               | 3,50 \n";
+menuBebida += "4 | Suco               | 7,50\n";
+menuBebida += "\n Digite o NUMERO da BEBIDA Escolhida: ";
+
+var numeroBebida = parseInt(prompt(menuBebida));
+
+var quantidade = parseInt(prompt("Quantas bebidas você deseja pedir? "));
+
+var totalBebida = 0
+
+if(numeroPizza == 1){
+    totalPizza = quantidade * 20.00;
+ }else if(numeroPizza == 2){
+     totalPizza = quantidade * 25.00;
+ }else if(numeroPizza == 3){
+     totalPizza = quantidade * 23.00;
+ }else if(numeroPizza == 4){
+     totalPizza = quantidade * 29.99;
+ }else if(numeroPizza == 5){
+     totalPizza = quantidade* 22.50;
+ }else{
+     console.log("Número de Pizza inválido!!");
+ }
+
+if(numeroBebida == 1){
+    totalBebida = quantidade * 5.00;
+}else if(numeroBebida == 2){
+    totalBebida = quantidade * 10.00;
+}else if(numeroBebida == 3){
+    totalBebida = quantidade * 3.50;
+}else if(numeroBebida == 4){
+    totalBebida = quantidade * 7.50;
+}else{
+    console.log("Número de Bebida inválido!!");
+}
+
+console.log("Seu pedido da pizza [ " + numeroPizza + " ]");
+console.log("Foram pedidos [ " + quantidade + " ] pizzas");
+console.log("Total da pizza:R$ " , totalPizza);
+
+console.log("Seu pedido da bebida [ " + numeroBebida + " ]");
+console.log("Foram pedidas [ " + quantidade + " ] bebidas");
+console.log("Total da bebida:R$ " , totalBebida);  
+
+console.log("Total a pagar:R$ " , totalBebida + totalPizza);
